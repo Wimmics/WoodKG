8df79a3834dd9ba0eac39b5fbd09bafba0582d5c
Sensaku <barbe.arnaud@hotmail.fr>
2023-03-09 15:12:01 +0100

    Push modification: add label to the build csv

--- iawa_extraction.py
+++ iawa_extraction.py
@@ -1,28 +1,39 @@
 import pandas as pd
 import re
 
+label = ["family", "genus", "species", "taxon", "author", "status", "accepted_id", "plant_id", "continent", "region", "area"]
+
 if __name__ == "__main__":
     # IAWA_Afrique_net
     df_iawa = pd.read_excel("wcvp_taxon_distri_anatomy_net.xlsx", sheet_name="IAWA_Afrique_net")
     print(df_iawa)
+
     extract_family = []
     extract_genre = []
-    extract_author = []
+    extract_usual_name = []
+
     for row in df_iawa.index:
         family = ""
         original_string = df_iawa["Taxa"][row].replace("?","")
         print(original_string)
-        #family = re.search("(PRIMULACEAE|LEGUMINOSAE|MALVACEAE)*\s[A-Z][A-Z]+", original_string).group(0)
-        family = re.search("[A-Z][A-Z]+\s[A-Z][A-Z]+|[A-Z][A-Z]+", original_string).group(0)
-        genre = re.search("[A-Z][a-z]+\s[a-z]+|[A-Z][a-z]+", original_string).group(0)
-        author = re.findall("\([A-Z][A-Z.]+\)\s[A-Z]+[A-Z.]", original_string)
-        extract_family.append(family)
-        extract_genre.append(genre)
-        extract_author.append(author)
+        for x in original_string.split("|"):
+            x = x.replace("Synonym:","")
+            #family = re.search("(PRIMULACEAE|LEGUMINOSAE|MALVACEAE)*\s[A-Z][A-Z]+", original_string).group(0)
+            family = re.search("[A-Z][A-Z]+\s[A-Z][A-Z]+|[A-Z][A-Z]+", x).group(0)
+            genre = re.search("[A-Z][a-z]+\s[a-z]+|[A-Z][a-z]+", x).group(0)
+            if re.search("\([A-Z][A-Z,\s]+\)", x):
+                usual_name = re.search("\([A-Z][A-Z,\s]+\)", x).group(0)
+            else:
+                usual_name = ""
+
+            extract_family.append(family)
+            extract_genre.append(genre)
+            extract_usual_name.append(usual_name)
+
     extract_family = list(set(extract_family))
     extract_genre = list(set(extract_genre))
     print(f"Nombre de famille : {len(extract_family)}\nList: {extract_family}")
     print(f"Nombre de genre : {len(extract_genre)}\nList: {extract_genre}")
-    print(f"Nombre d'author : {extract_author}\nList: {extract_author}")
+    print(f"Nombre d'author : {len(extract_usual_name)}\nList: {extract_usual_name}")
 
     #print(to_extract)
